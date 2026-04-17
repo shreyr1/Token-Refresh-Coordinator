1578c6b08c87ef1412abea574076bad2368e16d2
Shivam Rai <shivamrai70049@gmail.com>
2026-04-17 09:02:36 +0530

    Enable timestamps in user model schema
    Added timestamps option to user schema.

--- User/BACKEND/models/user.model.js
+++ User/BACKEND/models/user.model.js
@@ -38,6 +38,8 @@ const userSchema = new mongoose.Schema({
         type: Number,
         default: 0,
     },
+},{
+ timestamps : true
 })
 
 userSchema.statics.hashPassword = async function(password){
